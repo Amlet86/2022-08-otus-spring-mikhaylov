2e6aa7f900b5b7075be4f6df486e49b384ac354a
amlet <wecow@yandex.ru>
2022-03-12 21:17:02 +0300

    hw-1 part-3

---
+++ hw-1-boot-part/src/test/java/amlet/utility/QuestionConverterTest.java
@@ -0,0 +1,23 @@
+package amlet.utility;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.amlet.dto.QuestionDto;
+import ru.amlet.entity.Question;
+import ru.amlet.utility.QuestionConverter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Класс QuestionConverter")
+public class QuestionConverterTest {
+
+    private final QuestionConverter questionConverter = new QuestionConverter();
+
+    @Test
+    @DisplayName("метод convertQuestionFromQuestionDto преобразует объект класса QuestionDto в Question")
+    void shouldConvertQuestionDtoToQuestionClass() {
+        QuestionDto questionDto = new QuestionDto();
+        assertEquals(Question.class,
+                questionConverter.convertQuestion(questionDto).getClass());
+    }
+}
